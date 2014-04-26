cdb82c800fc9c6186fe8a53073eb9c5be8ef3576
Stan <ken.ensx@gmail.com>
2014-04-25 23:59:06 -0400

    Add external supporting files

---
+++ Autista/Classes/NSString+MD5Addition.h
@@ -0,0 +1,13 @@
+//
+//  NSString+MD5Addition.h
+//  Autista
+//
+//  Created by Haozhe Li on 4/25/14.
+//  Copyright (c) 2014 Shashwat Parhi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString_MD5Addition : NSObject
+
+@end
---
+++ Autista/Classes/NSString+MD5Addition.m
@@ -0,0 +1,13 @@
+//
+//  NSString+MD5Addition.m
+//  Autista
+//
+//  Created by Haozhe Li on 4/25/14.
+//  Copyright (c) 2014 Shashwat Parhi. All rights reserved.
+//
+
+#import "NSString+MD5Addition.h"
+
+@implementation NSString_MD5Addition
+
+@end
