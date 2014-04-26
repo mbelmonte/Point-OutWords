98c8ec359e9c2272071e5417e1117aa67f10ab92
Stan <ken.ensx@gmail.com>
2014-04-26 14:21:08 -0400

    Commit changes for xcodeproj

--- Autista/Classes/NSString+MD5Addition.h
+++ Autista/Classes/NSString+MD5Addition.h
@@ -1,13 +1,15 @@
 //
 //  NSString+MD5Addition.h
-//  Autista
+//  UIDeviceAddition
 //
-//  Created by Haozhe Li on 4/25/14.
-//  Copyright (c) 2014 Shashwat Parhi. All rights reserved.
+//  Created by Georg Kitz on 20.08.11.
+//  Copyright 2011 Aurora Apps. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-@interface NSString_MD5Addition : NSObject
+@interface NSString(MD5Addition)
+
+- (NSString *) stringFromMD5;
 
 @end
--- Autista/Classes/NSString+MD5Addition.m
+++ Autista/Classes/NSString+MD5Addition.m
@@ -1,13 +1,32 @@
 //
 //  NSString+MD5Addition.m
-//  Autista
+//  UIDeviceAddition
 //
-//  Created by Haozhe Li on 4/25/14.
-//  Copyright (c) 2014 Shashwat Parhi. All rights reserved.
+//  Created by Georg Kitz on 20.08.11.
+//  Copyright 2011 Aurora Apps. All rights reserved.
 //
 
 #import "NSString+MD5Addition.h"
+#import <CommonCrypto/CommonDigest.h>
 
-@implementation NSString_MD5Addition
+@implementation NSString(MD5Addition)
+
+- (NSString *) stringFromMD5{
+    
+    if(self == nil || [self length] == 0)
+        return nil;
+    
+    const char *value = [self UTF8String];
+    
+    unsigned char outputBuffer[CC_MD5_DIGEST_LENGTH];
+    CC_MD5(value, strlen(value), outputBuffer);
+    
+    NSMutableString *outputString = [[NSMutableString alloc] initWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    for(NSInteger count = 0; count < CC_MD5_DIGEST_LENGTH; count++){
+        [outputString appendFormat:@"%02x",outputBuffer[count]];
+    }
+    
+    return outputString;
+}
 
 @end
