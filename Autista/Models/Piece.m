9877e9acfb034c2e8326b4ef88cbe2ef25bb5871
Anhong Guo <guoanhong@gatech.edu>
2015-03-28 15:07:06 -0400

    Updated Copyright Notice, updated "Racket" in Speak Mode

--- Autista/Models/Piece.m
+++ Autista/Models/Piece.m
@@ -1,20 +1,14 @@
 //
 //  Piece.m
 //  Autista
+//  Autista is a tablet application to help autistic children with speech
+//  difficulties develop manual motor and oral motor skills.
 //
-//  Copyright (c) 2014 The Groden Center, Inc.
+//  Copyright (C) 2014 The Groden Center, Inc.
 //
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  To view the GNU General Public License, visit <http://www.gnu.org/licenses/>.
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
 
 #import "Piece.h"
--- Autista/Models/ProgressView.m
+++ Autista/Models/ProgressView.m
@@ -1,9 +1,14 @@
 //
 //  ProgressView.m
 //  Autista
+//  Autista is a tablet application to help autistic children with speech
+//  difficulties develop manual motor and oral motor skills.
 //
-//  Created by Shawn Wu on 4/27/14.
-//  Copyright (c) 2014 Shashwat Parhi. All rights reserved.
+//  Copyright (C) 2014 The Groden Center, Inc.
+//
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
 
 #import "ProgressView.h"
